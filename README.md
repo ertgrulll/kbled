ab4b5fb0c0857be10ed50b57e1463c01f3047660
ertgrulll <65064049+ertgrulll@users.noreply.github.com>
2021-03-02 18:45:57 +0300

    rel

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # kbled
-Ubuntu keyboard backlight controller 
+
+##### Ubuntu için klavye arka ışık denetleyicisi
+
+Ubuntu'da Scroll Lock tuşu ile klavye ışıklandırmasının etkinleştirilemediği için bu scripti oluşturdum.
+Sadece, Scroll Lock tuşu ile aydınlatması açılabilen klavyeler için yapıldı.
+Ayrıca, sadece arka ışığını açıp kapatma işlevi olan bir klavyeye sahipseniz, klavyenize 5 farklı efekt uygulayabilirsiniz.
+
+---
+
+##### Keyboard backlight controller for Ubuntu
+
+Scroll Lock key is not activating keyboard backlights on Ubuntu, so i created this script.
+Made for keyboards that enable backlighting with the scroll lock key.
+Also you can apply 5 different effects to an ordinary backlit led keyboard.
+
+---
+
+### Setting Up
+
+    - Download the file and place it desired location.(eg: ~/kbled.sh)
+    - Open terminal(ctrl+alt+t) and type: chmod +x path/to/file
+    - Add this to end of ~/.bashrc file as a new line: alias kbled='pkill -f "kbled.sh"; setsid path/to/file'
+        - In terminal: sudo nano ~/.bashrc
+        - source ~/.bashrc or . ~/.bashrc
+    - Now it's ready to use with one of theese commands:
+        - kbled help
+        - kbled on
+        - kbled off
+        - kbled heartbeat or kbled hb
+        - kbled reverseheartbeat or rhb
+        - kbled flash or kbled flash {speed} (you can set any number instead of {speed})
+        - kbled notice or kbled ntc
+        - kbled sine or kbled sn
---
+++ kbled.sh
@@ -0,0 +1,135 @@
+#!/bin/bash
+#AUTHOR: Ertugrul Yakin, https://ertugrulyakin.engineer
+
+OPT=$1
+freq=$2
+
+rhb () {
+ while true
+ do
+  xset -led named "Scroll Lock"
+  sleep 0.15s
+  xset led named "Scroll Lock"
+  sleep 0.1s
+  xset -led named "Scroll Lock"
+  sleep 0.15s
+  xset led named "Scroll Lock"
+  sleep 1.5s
+ done
+}
+
+hb () {
+ while true
+ do
+  xset led named "Scroll Lock"
+  sleep 0.2s
+  xset -led named "Scroll Lock"
+  sleep 0.1s
+  xset led named "Scroll Lock"
+  sleep 0.2s
+  xset -led named "Scroll Lock"
+  sleep 1.5s
+ done
+}
+
+flash () {
+ frequency=`echo "scale=1; 1/10" | bc -l`
+ if [[ $# -eq 1 ]];
+  then
+   frequency=`echo "scale=1; $1/20" | bc -l`
+fi
+ while true
+  do
+   xset -led named "Scroll Lock"
+   sleep $frequency
+   xset led named "Scroll Lock"
+   sleep $frequency
+ done
+}
+
+notice () {
+  while true
+  do
+   xset -led named "Scroll Lock"
+   sleep 2s
+   for ((i=0; i<20; i++))
+   do
+    xset led named "Scroll Lock" 
+    sleep .05s
+    xset -led named "Scroll Lock" 
+    sleep .05s
+   done
+  done
+}
+
+notice () {
+  while true
+  do
+   xset -led named "Scroll Lock"
+   sleep 2s
+   for ((i=0; i<20; i++))
+   do
+    xset led named "Scroll Lock" 
+    sleep .05s
+    xset -led named "Scroll Lock" 
+    sleep .05s
+   done
+  done
+}
+
+
+sine () {
+  while true
+  do
+   for ((i=41; i>1; i--))
+   do
+    sTime=`echo "scale=5; $i/200" | bc -l`
+    xset led named "Scroll Lock" 
+    sleep $sTime
+    xset -led named "Scroll Lock" 
+    sleep $sTime
+   done
+   for ((i=1; i<41; i++))
+   do
+    sTime=`echo "scale=5; $i/200" | bc -l`
+    xset led named "Scroll Lock" 
+    sleep $sTime
+    xset -led named "Scroll Lock" 
+    sleep $sTime
+   done
+   xset -led named "Scroll Lock"
+   sleep 2s
+  done
+}
+
+showHelp () {
+ echo -e "\n\033[38;5;75mUsage:\033[39m"
+ echo -e " * on\n * off\n * heartbeat || hb\n * reverseheartbeat || rhb\n * flash {speed, lowest is fastest}\n * notice || ntc\n * sine || sn"
+ echo -e "\033[38;5;75mEx:\033[39m"
+ echo " -> kbled on"
+ echo " -> kbled flash"
+ echo " -> kbled flash 5"
+ echo " -> kbled hb"
+ echo " -> kbled ntc"
+}
+
+if [[ "$OPT" == "heartbeat" ]] || [[ "$OPT" == "hb" ]]; then
+ hb
+elif [[ "$OPT" == "sine" ]] || [[ "$OPT" == "sn" ]]; then
+ sine
+elif [[ "$OPT" == "reverseheartbeat" ]] || [[ "$OPT" == "rhb" ]]; then
+ rhb
+elif [[ "$OPT" == "flash" ]]; then
+ flash $2
+elif [[ "$OPT" == "notice" ]] || [[ "$OPT" == "ntc" ]]; then
+ notice
+elif [[ "$OPT" == "on" ]]; then
+ xset led named "Scroll Lock"
+elif [[ "$OPT" == "off" ]] || [[ "$OPT" == "kill" ]]; then
+ xset -led named "Scroll Lock"
+ pkill -f "kbled"
+elif [[ "$OPT" == "help" ]]; then
+ showHelp
+else
+ showHelp
+fi
